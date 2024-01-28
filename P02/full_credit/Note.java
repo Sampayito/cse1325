eac5fbf47c58617fa826f1241526c9a5ac08ff15
Leonardo Sampayo <lxs3275@mavs.uta.edu>
2024-01-27 19:48:45 -0600

    updated note and pitch

--- P02/full_credit/Note.java
+++ P02/full_credit/Note.java
@@ -1,11 +1,29 @@
 public class Note {
 	private Pitch pitch;
 	private int octave;
-	Note();
-	Note(pitch, octave);
+	Note() {
+		pitch = null;
+	}
+	Note(pitch, octave) {
+		this.pitch = pitch;
+		if (octave < -5) {
+			this.octave = -5;
+		}
+		else if (octave > 4) {
+			this.octave = 4;
+		}
+	}
 	@Override
 	public String toString() {
-		
+		if (pitch.equals(null)) {
+			return " ";
+		}
+		if (octave.equals(0) {
+			return pitch;
+		}
+		else {
+			return pitch + octave;
+		}
 	}
 	
 }
