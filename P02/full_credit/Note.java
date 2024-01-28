0bd700e69bb0d4aee79ddfa34f81feacc3d3d3f2
Leonardo Sampayo <lxs3275@mavs.uta.edu>
2024-01-27 19:32:45 -0600

    started on the note class and pitch enum

---
+++ P02/full_credit/Note.java
@@ -0,0 +1,11 @@
+public class Note {
+	private Pitch pitch;
+	private int octave;
+	Note();
+	Note(pitch, octave);
+	@Override
+	public String toString() {
+		
+	}
+	
+}
---
+++ P02/full_credit/Pitch.java
@@ -0,0 +1,14 @@
+public enum Pitch {
+	C,
+	Db,
+	D,
+	Eb,
+	E,
+	F,
+	Gb,
+	G,
+	Ab,
+	A
+	Bb,
+	B;
+}
