70efb5fe3c5ac94fb12d551f274f5a182822afa0
Leonardo Sampayo <lxs3275@mavs.uta.edu>
2024-01-31 22:13:42 -0600

    completed Customer class and build.xml

---
+++ P03/Customer.java
@@ -0,0 +1,18 @@
+public class Customer {
+    private String name;
+    private String email;
+    
+    public String Customer(String name, String email) {
+        this.name = name;
+        if (email.contains("@") && email.contains(".") && (email.indexOf("@") < email.lastIndexOf(".")) {
+            throw new IllegalArgumentException("Invalid email address: " + email)
+        }
+        else {
+            this.email = email;
+        }
+    }
+    @Override
+    String toString() {
+        return "" + name + " (" + email + ")";
+    }
+}
