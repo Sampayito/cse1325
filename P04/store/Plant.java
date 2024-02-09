16c4787a5c3699140d181448d884cc683e8e25ab
Leonardo Sampayo <lxs3275@mavs.uta.edu>
2024-02-09 14:34:48 -0600

    completed plant subclass

--- P04/store/Plant.java
+++ P04/store/Plant.java
@@ -1,3 +1,11 @@
 public class Plant {
-
+    private Exposure exposure;
+    
+    public Plant(String species, int price, Exposure exposure) {
+        super("Plant: " + species, price);
+        this.exposure = exposure;
+    }
+    public Exposure getExposure(){
+        return exposure;
+    }
 }
