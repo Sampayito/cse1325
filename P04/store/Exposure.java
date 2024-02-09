43464baa41c619cb7ab53b3d5c8e45f797130a38
Leonardo Sampayo <lxs3275@mavs.uta.edu>
2024-02-09 14:24:42 -0600

    completed exposure and tool subclasses

---
+++ P04/store/Exposure.java
@@ -0,0 +1,12 @@
+public enum Exposure {
+    SHADE("full shade"), PARTSUN("part sun / part shade"), SUN("full sun");
+    private String string;
+    private Exposure(String string) {
+        this.string = string;
+    }
+    
+    @Override
+    public String toString() {
+        return string;
+    }
+}
