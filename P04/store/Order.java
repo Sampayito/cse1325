1ae55dfa2d0a42dc3dde30d2fb65a43139991805
Leonardo Sampayo <lxs3275@mavs.uta.edu>
2024-02-12 11:09:43 -0600

    added necessary components for javadoc

--- P04/store/Order.java
+++ P04/store/Order.java
@@ -1,20 +1,42 @@
 package store;
 import java.util.ArrayList;
 
+/**
+  * Encapsulation of a Customer and a set of items the Customer wants to buy.
+  *
+  * @author		Leonardo Sampayo
+  * @version		0.2
+  * @since		0.2
+  */
+
 public class Order {
     private static int nextOrderNumber = 1;
     private int orderNumber;
     private ArrayList<Item> items;
     private final Customer customer;
     
+/**
+  * @param		the Customer making the order
+  * @since		0.2
+  */
     public Order(Customer customer) {
         orderNumber = nextOrderNumber++;
 	this.customer = customer;
 	this.items = new ArrayList<>();
     }
+
+/**
+  * @param		the item being added to the items ArrayList
+  * @since		0.2
+  */
     public void addItem(Item item) {
         items.add(item);
     }
+
+/**
+  * @returns		sum of the price of all Items in the items ArrayList	
+  * @since		0.2
+  */
     public int getPrice() {
         int sum = 0;
         for (Item i: items) {
@@ -22,6 +44,11 @@ public class Order {
         }
         return sum;
     }
+
+/**
+  * @returns		a String formatted like a receipt	
+  * @since		0.2
+  */
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
