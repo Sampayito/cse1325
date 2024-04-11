eb77372fb4917f1dcb2c303363e8db5806076aa5
Leonardo Sampayo <lxs3275@mavs.uta.edu>
2024-04-10 20:37:47 -0500

    inch.h

---
+++ P10/full_credit/inch.h
@@ -0,0 +1,26 @@
+#ifndef __INCH_H
+#define __INCH_H
+
+class Inch {
+  public:
+    Inch();
+    Inch(int whole, int numerator, int denominator);
+    Inch operator+(const Inch& rhs);
+    bool operator==(const Inch& rhs);
+    bool operator!=(const Inch& rhs);
+    bool operator<(const Inch& rhs);
+    bool operator<=(const Inch& rhs);
+    bool operator>(const Inch& rhs);
+    bool operator>=(const Inch& rhs);
+    friend std::ostream& operator<<(std::ostream& ost, const Inch& inch);
+    friend std::istream& operator>>(std::istream& ist, Inch& inch);
+  private:
+    const int compare(const Inch& rhs);
+    void validate();
+  
+    int _whole;
+    int _numerator;
+    int _denominator;
+};
+
+#endif
