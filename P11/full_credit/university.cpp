7124e60b0bd1ad2289dc6249ede4a31381d91c42
Leonardo Sampayo <lxs3275@mavs.uta.edu>
2024-04-18 13:51:35 -0500

    worked on university.h and .cpp

---
+++ P11/full_credit/university.cpp
@@ -0,0 +1,33 @@
+#include "university.h"
+
+University::University(std::string name = "", int enrollment = 0) 
+    : _name{name}, _enrollment{enrollment} {
+    validate();
+} 
+
+std::string University::name() const {
+    return _name;
+}
+    
+int University::enrollment() const {
+     return _enrollment;
+}
+    
+std::istream& operator>>(std::istream& ist, University& reading) {
+    ist >> reading._name >> reading._enrollment;
+    reading.validate();
+    return ist;
+}
+    
+std::ostream& operator<<(std::ostream& ost, const University& reading) {
+    return ost << reading._name << " has an enrollment of " << reading._enrollment;
+}
+
+void University::validate() {
+    if (_enrollemnt < 0) {
+        throw std::invalid_argument("University enrollment cannot be negative");
+    }
+    if (_enrollment > 0 && _name.length() == 0) {
+        throw std::invalid_argument("University cannot be anonymous");
+    }
+}
---
+++ P11/full_credit/university.h
@@ -0,0 +1,37 @@
+#ifndef __UNIVERSITY_H
+#define __UNIVERSITY_H
+
+#include <iostream>
+
+class University {
+  public:
+    University(std::string name, int enrollment);
+    
+    std::string name() const {
+        return _name;
+    }
+    
+    int enrollment() const {
+        return _enrollment;
+    }
+    
+    std::istream& operator>>(std::istream& ist, University& reading) {
+        ist >> reading._name >> reading._enrollment;
+        reading.validate();
+        return ist;
+    }
+    
+    std::ostream& operator<<(std::ostream& ost, const University& reading) {
+        return ost << _name << " has an enrollment of " << _enrollment;
+    }
+    
+    std::string name() const;
+    int enrollment() const;
+    friend std::istream& operator>>(std::istream& ist, University& reading);
+    friend std::ostream& operator<<(std::ostream& ost, const University& reading);
+  
+  private:
+    void validate();
+    std::string _name;
+    int _enrollment;
+}
